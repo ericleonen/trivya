8ed321c2a4e882272656984b9b61ea4125a046b3
ericleonen <ericleonen@gmail.com>
2022-04-10 20:15:58 -0700

    Initialize firebase

---
+++ src/firebase/index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCJsxRZgbkULS8ysJh9Z7CD7twPsNzr1rM",
+  authDomain: "trivya-177cf.firebaseapp.com",
+  projectId: "trivya-177cf",
+  storageBucket: "trivya-177cf.appspot.com",
+  messagingSenderId: "25513986704",
+  appId: "1:25513986704:web:0fee046523fdffa395e615"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
