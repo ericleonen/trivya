3e7286e8679625d5f8827edebeff01fa7d3c301e
ericleonen <ericleonen@gmail.com>
2022-04-11 21:23:45 -0700

    Add TopBar functionality to customize

--- src/commons/TopBar/index.js
+++ src/commons/TopBar/index.js
@@ -1,9 +1,37 @@
 import './TopBar.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeft, faXmark, faTrophy, faCoins } from '@fortawesome/free-solid-svg-icons';
 
 const TopBar = ({ text, showProfile, showTrophies, showCoins, showBackArrow, showExitX }) => {
     return (
         <div className="TopBar">
+            { 
+                showBackArrow && 
+                <button className="top-bar-btn">
+                    <FontAwesomeIcon icon={faArrowLeft} />
+                </button> 
+            }
+            { 
+                showExitX && 
+                <button className="top-bar-btn">
+                    <FontAwesomeIcon icon={faXmark} />
+                </button> 
+            }
             <h2>{text}</h2>
+            { 
+                showTrophies && 
+                <div className="top-bar-stat">
+                    <FontAwesomeIcon icon={faTrophy} />
+                    7,890
+                </div> 
+            }
+            { 
+                showCoins && 
+                <div className="top-bar-stat">
+                    <FontAwesomeIcon icon={faCoins} />
+                    872
+                </div> 
+            }
         </div>
     );
 };
